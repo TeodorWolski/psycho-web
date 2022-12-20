2ddea594718ce41a51af92d9fa5a071ea9247bf6
TeodorWolski <teodor.wolski@dreamstormstudios.com>
2022-12-20 17:07:57 +0100

    feat: useMobile hook

---
+++ src/hooks/useMobile.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+export const useMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isMobile]);
+
+  return { isMobile };
+};
