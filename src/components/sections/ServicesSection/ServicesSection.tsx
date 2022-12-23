efd9e8eb22d1ad718e3030c2b9bc63eb56a9666a
TeodorWolski <teodorwolski88@gmail.com>
2022-12-23 13:06:04 +0100

    feat: ServiceSection mock-up

--- src/components/sections/ServicesSection/ServicesSection.tsx
+++ src/components/sections/ServicesSection/ServicesSection.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { Text } from 'components/atoms/Text';
 
 export const ServicesSection = () => {
-  return <div className="bg-secondary h-60 w-full mt-12">ServicesSection</div>;
+  return (
+    <div className="bg-secondary h-72 w-full mt-1 custom-radius flex  items-center justify-center flex-col">
+      <section className="p-3">
+        <Text color="primary" size="lg" custom="font-bold">
+          Profesjonalna pomoc 🛟
+        </Text>
+        <Text size="sm">
+          lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit.
+        </Text>
+      </section>
+      <section className="p-3">
+        <Text size="lg" custom="font-bold" color="primary">
+          Oczyszczenie umysłu 🧘‍♀️
+        </Text>
+        <Text size="sm">
+          lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit.
+        </Text>
+      </section>
+      <section className="p-3">
+        <Text color="primary" size="lg" custom="font-bold">
+          Wewnętrzny spokój 🍃
+        </Text>
+        <Text size="sm">
+          lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit.
+        </Text>
+      </section>
+    </div>
+  );
 };
