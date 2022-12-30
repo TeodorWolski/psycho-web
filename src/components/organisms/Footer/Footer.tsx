f46ec8ebde70e4f9e16ea048be9246bf0e1db2d6
TeodorWolski <teodorwolski88@gmail.com>
2022-12-30 17:52:55 +0100

    feat: added XS rwd for footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,33 +1,28 @@
 import React from 'react';
 import { Text } from 'components/atoms/Text';
+import { content } from './content';
+
 export const Footer = () => {
   return (
-    <footer className="bg-tertiary text-white p-3 mt-5">
+    <footer className="bg-tertiary text-white p-3 mt-5 w-full flex flex-col align-center justify-center">
       <Text size="lg" custom="font-bold text-center">
         Kontakt
       </Text>
-      <section className="mt-3">
-        <Text size="md" custom="font-medium py-1">
-          Tadeusz Norek
-        </Text>
-        <Text size="sm">
-          tel. kontaktowy: 📞 <strong>123 456 789</strong>
-        </Text>
-        <Text size="sm">
-          e-mail: 📩 <strong>mail@example.com</strong>
-        </Text>
-      </section>
-      <section className="my-3">
-        <Text size="md" custom="font-medium py-1">
-          Christian Kubota
-        </Text>
-        <Text size="sm">
-          tel. kontaktowy: 📞 <strong>789 654 321</strong>
-        </Text>
-        <Text size="sm">
-          e-mail: 📩 <strong>liam@example.com</strong>
-        </Text>
-      </section>
+      <div className="flex w-full justify-center px-5 flex-col custom-xs:flex-row">
+        {content.map(({ name, phone, email }) => (
+          <section className="my-3 px-5" key={name}>
+            <Text size="md" custom="font-medium py-1">
+              {name}
+            </Text>
+            <Text size="sm">
+              tel. kontaktowy: 📞 <strong>{phone}</strong>
+            </Text>
+            <Text size="sm">
+              e-mail: 📩 <strong>{email}</strong>
+            </Text>
+          </section>
+        ))}
+      </div>
     </footer>
   );
 };
