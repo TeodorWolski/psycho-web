0f81d711b77f72ff4c22ae27444024d258dd6b0a
TeodorWolski <teodorwolski88@gmail.com>
2022-12-30 17:53:40 +0100

    ref: exported content

---
+++ src/components/organisms/Footer/content.ts
@@ -0,0 +1,12 @@
+export const content = [
+  {
+    name: 'Tadeusz Norek',
+    phone: '123 456 789',
+    email: 'mail@example.com',
+  },
+  {
+    name: 'Christian Kubota',
+    phone: '789 654 321',
+    email: 'liam@example.com',
+  },
+];
