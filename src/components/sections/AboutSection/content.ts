6aa4dc77e3a4500cc2a760a1c459ea8bcd1c7087
TeodorWolski <teodorwolski88@gmail.com>
2022-12-30 17:56:19 +0100

    feat: added XS rwd for AboutSection and small ref

---
+++ src/components/sections/AboutSection/content.ts
@@ -0,0 +1,16 @@
+export const content = [
+  {
+    name: 'Tadeusz Norek',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, nihil.',
+    src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthedogman.net%2Fwp-content%2Fuploads%2F2020%2F10%2F49068634038_31d19cd6a6_k-1.jpg%3Fx50647&f=1&nofb=1&ipt=512e33e4c232de9299ea33653f6496bd43aca3227894e404fa209985ba5f7c68&ipo=images',
+    alt: 'Tadeusz Norek photo',
+  },
+  {
+    name: 'Tomasz O Leary',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, nihil.',
+    alt: 'Tomasz O Leary photo',
+    src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthedogman.net%2Fwp-content%2Fuploads%2F2020%2F10%2F49068634038_31d19cd6a6_k-1.jpg%3Fx50647&f=1&nofb=1&ipt=512e33e4c232de9299ea33653f6496bd43aca3227894e404fa209985ba5f7c68&ipo=images',
+  },
+];
