7c30817fb6451239dc72860186c6f5ce9e6a3563
TeodorWolski <teodorwolski88@gmail.com>
2022-12-30 17:58:48 +0100

    feat: added XS rwd for HeroSection

--- src/components/sections/HeroSection/HeroSection.tsx
+++ src/components/sections/HeroSection/HeroSection.tsx
@@ -5,24 +5,24 @@ import { Text } from 'components/atoms/Text';
 
 export const HeroSection = () => {
   return (
-    <section className="mx-3 my-5 h-72 h-full flex w-100 align-center justify-around">
-      <div className="w-3/4 flex flex-col flex-end">
-        <Text custom="p-2 font-medium" size="lg">
+    <section className="mx-3 my-5 h-72 h-full flex w-100 align-center justify-around custom-xs:justify-center custom-xs:h-80 custom-xs:my-0">
+      <div className="w-3/4 flex flex-col flex-end custom-xs:justify-center">
+        <Text custom="p-2 font-medium custom-xs:text-xl" size="lg">
           Lorem ipsum dolor sit amet,{' '}
           <strong className="font-extrabold text-primary">
             {' '}
             consectetur adipiscing elit.
           </strong>
         </Text>
-        <Text size="sm" custom="w-full p-2">
+        <Text custom="w-full p-2" size="sm">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
           in nobis a soluta tenetur
         </Text>
-        <button className="font-extrabold text-xs px-10 py-3 mt-3 mr-auto text-white bg-tertiary  shadow-black rounded-3xl">
+        <button className="font-extrabold text-xs px-10 py-3 mt-3 mr-auto text-white bg-tertiary shadow-black rounded-3xl  custom-xs:text-sm">
           Click me
         </button>
       </div>
-      <div className=" w-1/2 flex flex-col items-center  ">
+      <div className=" w-1/2 flex flex-col items-center  custom-xs:justify-center">
         {ClientInfo.map(({ src, alt, reverse }) => (
           <Avatar src={src} alt={alt} reverse={reverse} key={alt} />
         ))}
