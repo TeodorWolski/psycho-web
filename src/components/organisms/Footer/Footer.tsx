1793fc0e7265935962b70b91e3cbedc20c271c64
TeodorWolski <teodorwolski88@gmail.com>
2023-01-02 13:06:03 +0100

    feat: sm RWD for Footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -4,20 +4,20 @@ import { content } from './content';
 
 export const Footer = () => {
   return (
-    <footer className="bg-tertiary text-white p-3 mt-5 w-full flex flex-col align-center justify-center">
-      <Text size="lg" custom="font-bold text-center">
+    <footer className="bg-tertiary text-white p-3 mt-5 w-full flex flex-col align-center justify-center custom-sm:py-8">
+      <Text size="lg" custom="font-bold text-center custom-sm:text-xl">
         Kontakt
       </Text>
-      <div className="flex w-full justify-center px-5 flex-col custom-xs:flex-row">
+      <div className="flex w-full items-center justify-around px-5 flex-col custom-xs:flex-row">
         {content.map(({ name, phone, email }) => (
-          <section className="my-3 px-5" key={name}>
-            <Text size="base" custom="font-medium py-1">
+          <section className="my-3 px-5 custom-sm:px-8" key={name}>
+            <Text size="base" custom="font-medium py-1 custom-sm:text-lg">
               {name}
             </Text>
-            <Text size="sm">
+            <Text size="sm" custom="custom-sm:text-base">
               tel. kontaktowy: 📞 <strong>{phone}</strong>
             </Text>
-            <Text size="sm">
+            <Text size="sm" custom="custom-sm:text-base">
               e-mail: 📩 <strong>{email}</strong>
             </Text>
           </section>
