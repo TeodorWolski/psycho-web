49d5313bae76590afe16f359ec682eaa13214536
TeodorWolski <teodorwolski88@protonmail.com>
2023-02-16 16:51:38 +0100

    feat: lg screens footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -11,32 +11,38 @@ export const Footer = () => {
     <footer
       ref={contactRef}
       className="bg-tertiary text-white p-3 mt-5 w-full flex flex-col align-center justify-center custom-sm:py-8">
-      <Text size="lg" custom="font-bold text-center custom-sm:text-xl">
+      <Text
+        size="lg"
+        custom="font-bold text-center custom-sm:text-xl custom-lg:text-2xl">
         Kontakt
       </Text>
-      <div className="flex w-full items-center justify-around flex-col custom-sm:flex-row custom-xs:px-5">
+      <div className="flex w-full items-center justify-around flex-col custom-sm:flex-row custom-xs:px-5 custom-lg:justify-center">
         {content.map(({ name, phone, email, price }) => (
           <section
-            className="my-3 w-full px-5 custom-sm:px-8 flex flex-col "
+            className="my-3 w-full px-5 custom-sm:px-8 flex flex-col custom-lg:items-center custom-lg:justify-center custom-lg:custom-sm:w-1/2"
             key={name}>
-            <Text size="base" custom="font-medium py-1 custom-sm:text-lg">
+            <Text
+              size="base"
+              custom="font-medium py-1 custom-sm:text-lg custom-lg:text-xl">
               {name}
             </Text>
             <Link
-              className="text-xs custom-xs:text-sm custom-sm:text-base"
+              className="text-xs custom-xs:text-sm custom-sm:text-base custom-lg:text-lg"
               href={`tel:${phone}`}
               target="_blank"
               rel="noopener noreferrer">
               📞 tel. kontaktowy: <strong>{phone}</strong>
             </Link>
             <Link
-              className="text-xs custom-xs:text-sm custom-sm:text-base"
+              className="text-xs custom-xs:text-sm custom-sm:text-base custom-lg:text-lg"
               href={`mailto:${email}`}
               target="_blank"
               rel="noopener noreferrer">
               📩 e-mail: <strong>{email}</strong>
             </Link>
-            <Text size="xs" custom="custom-xs:text-sm custom-sm:text-base">
+            <Text
+              size="xs"
+              custom="custom-xs:text-sm custom-sm:text-base custom-lg:text-lg">
               💳 cena: <strong>{price}</strong>
             </Text>
           </section>
