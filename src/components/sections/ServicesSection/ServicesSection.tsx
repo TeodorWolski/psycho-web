01621ff906383b1e87743f5af4029ceca913191c
TeodorWolski <teodorwolski88@protonmail.com>
2023-02-16 16:53:39 +0100

    feat: xl screens services

--- src/components/sections/ServicesSection/ServicesSection.tsx
+++ src/components/sections/ServicesSection/ServicesSection.tsx
@@ -7,7 +7,7 @@ export const ServicesSection = () => {
     <section className="bg-secondary h-80 w-full custom-radius flex items-center justify-center flex-col custom-sm:h-96 custom-md:flex-row custom-md:h-52">
       {content.map(({ title, description }) => (
         <div
-          className="py-2 custom-xs:p-5 custom-sm:py-6 custom-sm:px-7 custom-md:px-4 custom-md:p-10 custom-md:h-full custom-md:w-1/3 custom-md:justify-center custom-md:items-center custom-md:flex custom-md:flex-col custom-md:items-center"
+          className="py-2 custom-xs:p-5 custom-sm:py-6 custom-sm:px-7 custom-md:px-4 custom-md:p-10 custom-md:h-full custom-md:w-1/3 custom-md:justify-center custom-md:items-center custom-md:flex custom-md:flex-col custom-md:items-center custom-xl:max-w-xl"
           key={title}>
           <Text
             color="primary"
