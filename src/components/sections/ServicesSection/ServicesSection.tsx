4e85b2d129b7615d601fabfb7a5d84ead167b0eb
TeodorWolski <teodorwolski88@protonmail.com>
2023-02-16 16:21:29 +0100

    fix: paddings

--- src/components/sections/ServicesSection/ServicesSection.tsx
+++ src/components/sections/ServicesSection/ServicesSection.tsx
@@ -4,20 +4,20 @@ import { content } from './content';
 
 export const ServicesSection = () => {
   return (
-    <section className="bg-secondary h-80 w-full custom-radius flex items-center justify-center flex-col custom-sm:h-96 custom-md:flex-row custom-md:h-60 px-5">
+    <section className="bg-secondary h-80 w-full custom-radius flex items-center justify-center flex-col custom-sm:h-96 custom-md:flex-row custom-md:h-52">
       {content.map(({ title, description }) => (
         <div
-          className="py-2 custom-xs:p-5 custom-sm:py-6 custom-sm:px-7 custom-md:px-2 custom-md:w-2/3  custom-md:justify-center custom-md:items-center custom-md:flex custom-md:flex-col custom-md:items-center"
+          className="py-2 custom-xs:p-5 custom-sm:py-6 custom-sm:px-7 custom-md:px-4 custom-md:p-10 custom-md:h-full custom-md:w-1/3 custom-md:justify-center custom-md:items-center custom-md:flex custom-md:flex-col custom-md:items-center"
           key={title}>
           <Text
             color="primary"
             size="base"
-            custom="font-semibold custom-sm:text-lg custom-md:py-2 custom-md:text-center">
+            custom="font-semibold custom-sm:text-lg custom-md:py-0 custom-md:text-center w-100 custom-md:h-30 mb-2">
             {title}
           </Text>
           <Text
             size="xs"
-            custom="font-normal custom-sm:text-sm custom-md:text-center">
+            custom="font-normal custom-sm:text-sm custom-md:text-center h-full">
             {description}
           </Text>
         </div>
